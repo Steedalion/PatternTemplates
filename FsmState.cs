1dcd6bc795153f373012156cf29a412cc79344fc
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-04-15 17:22:25 +0900

    Initial commit

---
+++ FsmState.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Patterns
+{
+    public class FsmState<TStateAsEnum, TSelfClass> where TStateAsEnum : Enum where TSelfClass : FsmState<TStateAsEnum,TSelfClass>
+    {
+        protected FsmStage Stage;
+        private TSelfClass nextState;
+        protected TStateAsEnum StateName;
+
+        protected virtual void Enter()
+        {
+            Stage = FsmStage.Update;
+        }
+
+        protected virtual void StateUpdate()
+        {
+            
+        }
+
+        protected virtual void Exit()
+        {
+            Stage = FsmStage.Exit;
+        }
+
+        protected void ProceedToNextStage(TSelfClass next)
+        {
+            nextState = next;
+            Stage = FsmStage.Exit;
+        }
+
+        protected void Initialize()
+        {
+            Stage = FsmStage.Enter;
+        }
+
+        public TSelfClass Process()
+        {
+            if (Stage == FsmStage.Enter) Enter();
+            if (Stage == FsmStage.Update) StateUpdate();
+            if (Stage != FsmStage.Exit) return (TSelfClass) this;
+            Exit();
+            return nextState;
+        }
+
+        protected enum FsmStage
+        {
+            Enter,
+            Update,
+            Exit
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,15 @@
+﻿namespace Patterns
+{
+    public class Singleton<T> where T: new()
+    {
+        private static T instance;
+        public static T Instance()
+        {
+            if (instance ==null)
+            {
+             instance = new T();
+            }
+            return instance;
+        }
+    }
+}
