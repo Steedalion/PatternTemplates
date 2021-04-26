4f086a9b4d39927525c6569cd7f471daf7873814
Steedalion <alex.steed88@gmail.com>
2021-04-26 23:25:09 +0900

    Singleton Mono returned. It needed to be referenced in tests.

---
+++ UnityTools/SingletonMonoBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T:SingletonMonoBehaviour<T>
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError(this + "Trying to instance a second instance of singleton");
+        }
+        else
+        {
+            Instance = (T) this;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (Instance == null) Instance = null;
+    }
+}
