b0f085958384d5fa69db7f0833d10e3d14cc250e
Steedalion <alex.steed88@gmail.com>
2021-04-26 22:52:18 +0900

    Added some tests for ObjectPool.

---
+++ Tools/ObjectPool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Toolbox.Tools
+{
+    public class ObjectPool<T> 
+    {
+        private T[] pool;
+        public int PoolSize { get; }
+
+        public ObjectPool(int poolSize)
+        {
+            PoolSize = poolSize;
+            pool = new T[poolSize];
+        }
+
+        public T GetItem(int index)
+        {
+            if (index > PoolSize) throw new PoolOutOfBoundException("Trying to get at index larger than pool size");
+            return pool[index];
+        }
+
+        public void SetItem(int index, T item)
+        {
+             if (index > PoolSize) throw new PoolOutOfBoundException("Trying to get at index larger than pool size");
+             pool[index] = item;
+        }
+
+    }
+
+    public class PoolOutOfBoundException : Exception
+    {
+        public PoolOutOfBoundException()
+        {
+        }
+
+        public PoolOutOfBoundException(string message) : base(message)
+        {
+        }
+
+        public PoolOutOfBoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Unity/ObjectPool.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Patterns
+{
+    public class ObjectPool<T>
+    {
+        private T[] pool;
+        public int PoolSize;
+        public ObjectPool(int size)
+        {
+            PoolSize = size;
+            pool = new T[size];
+        }
+
+        public T GetItem(int i)
+        {
+            return pool[i];
+        }
+
+        public void SetItem(int index, T item)
+        {
+            pool[index] = item;
+        }
+    }
+}
