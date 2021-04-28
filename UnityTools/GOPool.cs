17beed50664d074ff779c40b6e01fca9ba775d15
Steedalion <alex.steed88@gmail.com>
2021-04-28 22:59:38 +0900

    Moved GameObjectPool to Toolbox.

---
+++ UnityTools/GOPool.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityToolbox.Tools;
+
+public class GOPool : MonoBehaviour
+{
+    public GameObject[] types;
+    public int poolSize = 50;
+    private ObjectPool<GameObject> pool;
+
+    private void Awake()
+    {
+        pool = new ObjectPool<GameObject>(poolSize);
+    }
+
+    private void Start()
+    {
+        CreatePool();
+    }
+
+    public GameObject PopDeactivatedObjectOrReturnNull()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject item = pool.GetItem(i);
+            if (!item.activeInHierarchy)
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    private void CreatePool()
+    {
+        int numberOfTypes = types.Length;
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject currentPrefab = types[i % numberOfTypes];
+            GameObject item = Instantiate(currentPrefab);
+            item.SetActive(false);
+            pool.SetItem(i, item);
+        }
+    }
+}
---
+++ UnityTools/ToolboxPlayTests/GOPoolTest.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests_PlayMode
+{
+    public class GOPoolTest
+    {
+        private GameObject gameObject;
+        
+         [UnityTearDown]
+        public IEnumerator DestroyCreatedObjects()
+        {
+            GameObject.Destroy(gameObject);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator SingleTypeSingleObject()
+        {
+            GOPool goPool = BuildPoolOfSingleType(new GameObject(),1);
+            yield return null;
+         
+            GameObject fromPool =  goPool.PopDeactivatedObjectOrReturnNull();
+            
+            Assert.IsNotNull(fromPool);
+            yield return null;
+        }
+        
+        [UnityTest]
+         public IEnumerator SingleTypeMultipleSingleObjects()
+         {
+             int poolsize = 10;
+            GOPool goPool = BuildPoolOfSingleType(new GameObject(),10);
+            yield return null;
+
+            for (int i = 0; i < poolsize; i++)
+            {
+                GameObject fromPool =  goPool.PopDeactivatedObjectOrReturnNull(); 
+                Assert.IsNotNull(fromPool, "Expected not null @ "+i); 
+                fromPool.SetActive(true);
+            }
+            
+            yield return null;
+        }    
+         public IEnumerator SingleTypeMultipleObjects()
+        {
+                        int poolsize;
+
+            GOPool goPool = BuildPoolOfSingleType(new GameObject(),1);
+            yield return null;
+         
+            GameObject fromPool =  goPool.PopDeactivatedObjectOrReturnNull();
+            
+            Assert.IsNotNull(fromPool);
+            yield return null;
+        }
+
+        private GOPool BuildPoolOfSingleType(GameObject type, int size)
+        {
+            GameObject[] singleType = new GameObject[1];
+            singleType[0] = type;
+            GOPool goPool = BuildPool(singleType, size);
+            return goPool;
+        }
+
+        private GOPool BuildPool(GameObject[] prefabs, int poolsize)
+        {
+            gameObject = GameObject.Instantiate(new GameObject());
+            GOPool goPool = gameObject.AddComponent<GOPool>();
+            
+            goPool.types = prefabs;
+            goPool.poolSize = poolsize;
+            return goPool;
+        }
+
+       
+    }
+}
